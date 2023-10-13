f0b11c7ed31a46002ece679851424fdf589b3c36
longlb1 <longlb@seatechit.com.vn>
2023-10-13 15:26:15 +0700

    Init projects 2 (LangChain FastAPI Streaming)

---
+++ LangChain-FastAPI-Streaming/test.py
@@ -0,0 +1,16 @@
+import os
+from dotenv import load_dotenv
+from langchain.chat_models import ChatOpenAI
+from langchain.schema import HumanMessage
+
+load_dotenv()
+
+from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
+
+chat = ChatOpenAI(
+    openai_api_key=os.getenv("OPENAI_API_KEY"),
+    streaming=True, 
+    callbacks=[StreamingStdOutCallbackHandler()], 
+    temperature=0
+)
+print(chat([HumanMessage(content="Write me a song about sparkling water.")]))
